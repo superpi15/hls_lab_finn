7d25b7be5f88b9920ff4f27fad13c70e3fe388fe
Nero Horus <superpi152@gmail.com>
2021-11-28 15:35:17 +0800

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,7 @@
+source /tools/Xilinx/Vitis/2020.1/settings64.sh 
+export FINN_XILINX_PATH=/tools/Xilinx/
+export FINN_XILINX_VERSION=2020.1
+export VIVADO_PATH=/tools/Xilinx/Vivado/2020.1
+export PYNQ_BOARD=Pynq-Z2
+export FINN_HOST_BUILD_DIR=/home/${USER}/build
+export PYNQ_IP=<YOUR_FPGA_IP>
